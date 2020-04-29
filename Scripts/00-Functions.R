d0f451ca277386e9731353096c86c8e5b25886ea
Roxanne Beauclair <roxannebeauclair@Roxannes-MacBook-Pro-2.local>
2020-04-29 14:15:13 +0200

    Update recap_bi_catvar_svy() "beta CIs and dropped unused levels

--- Scripts/00-Functions.R
+++ Scripts/00-Functions.R
@@ -149,10 +149,13 @@ recap_uni_numvar_svy <- function(svy, var) {
 # corrections to the Pearson chisquared test (defaults "F")
 # This function returns frequencies (Freq) and proportions (Prop)
 # and 95% intervals 
-# The percent argument specifies if you want the percents to be as 
-# a fraction of the column variable ("col") or row variable ("row")
+# The proportions are calculated using svyciprop() from the survey
+# package. Specifically, I use the "beta" method. This method
+# uses the incomplete beta function as in binom.test(), with 
+# an effective sample size based on the estimated variance
+# of the proportion. Allows more accurate intervals near 0 and 100%
 
-recap_bi_catvar_svy <- function(svy, var, byvar, percent = "col") {
+recap_bi_catvar_svy <- function(svy, var, byvar) {
   
   df <- svy
   rowvar <- enquo(var)
@@ -161,51 +164,62 @@ recap_bi_catvar_svy <- function(svy, var, byvar, percent = "col") {
   colvar_str <- quo_name(colvar)
   formula <- as.formula(paste("~", rowvar_str, "+", colvar_str))
   
-  overall_tab <- df %>%
-    group_by(!! rowvar, .drop = FALSE) %>%
-    summarise(Freq = unweighted(n()),
-              Prop = survey_mean(na.rm = T,
-                                 vartype = "ci")) %>%
+  # Overall table
+  c_levels <- unique(df$variables[[rowvar_str]])
+  
+  overall_tab_n <- df %>%
+    group_by(!! rowvar, .drop = TRUE) %>%
+    summarise(Freq = unweighted(n()))
+  
+  overall_tab_prop <- map_df(c_levels, function(cat) {
+    df %>%
+      summarise(Prop = survey_mean(!! rowvar == cat,
+                                   na.rm = T,
+                                   vartype = "ci",
+                                   proportion = TRUE, 
+                                   prop_method = "beta")) %>%
+      mutate(!! rowvar_str := cat) # := is a helper for creating new names
+    
+  })
+  
+  overall_tab_prop <- overall_tab_prop %>%
+    mutate_at(vars(starts_with("Prop")), 
+              list(~format(round(. * 100, 1), nsmall = 1))) %>%
+    unite(CI, Prop_low, Prop_upp, sep = " — ") 
+  
+  overall_tab <- overall_tab_n %>%
+    left_join(overall_tab_prop, by = rowvar_str) %>%
+    rownames_to_column()
+  
+  # By Table
+  by_tab_n <- df %>%
+    group_by(!! colvar, !!rowvar, .drop = TRUE) %>%
+    summarise(Freq = unweighted(n()))
+  
+  
+  by_tab_prop <- map_df(c_levels, function(cat)
+    df %>%
+      group_by(!! colvar) %>%
+      summarise(Prop = survey_mean(!! rowvar == cat,
+                                   na.rm = T,
+                                   vartype = "ci",
+                                   proportion = TRUE, 
+                                   prop_method = "beta")) %>%
+      mutate(!! rowvar_str := cat)
+  )
+  
+  by_tab <- by_tab_n %>%
+    left_join(by_tab_prop, by = c(colvar_str, rowvar_str)) %>%
     mutate_at(vars(starts_with("Prop")), 
               list(~format(round(. * 100, 1), nsmall = 1))) %>%
     unite(CI, Prop_low, Prop_upp, sep = " — ") %>%
-    rownames_to_column() # Need to do this so can join p-value table (wrong number of rows)
-                         # for bind_cols
+    gather(key = "stat", value = "value", Freq, Prop, CI) %>%
+    unite(var_stat, !! colvar, stat, sep = "_") %>%
+    spread(key = var_stat, value = value) %>%
+    select(!! rowvar, No_Freq, No_Prop, No_CI, Yes_Freq, Yes_Prop, Yes_CI) %>%
+    rownames_to_column() 
   
-  if(percent == "col") {
-    
-    by_tab <- df %>%
-      group_by(!! colvar, !! rowvar, .drop = FALSE) %>%
-      summarise(Freq = unweighted(n()),
-                Prop = survey_mean(na.rm = T,
-                                   vartype = "ci")) %>%
-      mutate_at(vars(starts_with("Prop")), 
-                list(~format(round(. * 100, 1), nsmall = 1))) %>%
-      unite(CI, Prop_low, Prop_upp, sep = " — ") %>%
-      gather(key = "stat", value = "value", Freq, Prop, CI) %>%
-      unite(var_stat, !! colvar, stat, sep = "_") %>%
-      spread(key = var_stat, value = value) %>%
-      select(!! rowvar, No_Freq, No_Prop, No_CI, Yes_Freq, Yes_Prop, Yes_CI) %>%
-      rownames_to_column() 
-    
-  } else {
-    
-    by_tab <- df %>%
-      group_by(!! rowvar, !! colvar, .drop = FALSE) %>%
-      summarise(Freq = unweighted(n()),
-                Prop = survey_mean(na.rm = T,
-                                   vartype = "ci")) %>%
-      mutate_at(vars(starts_with("Prop")), 
-                list(~format(round(. * 100, 1), nsmall = 1))) %>%
-      unite(CI, Prop_low, Prop_upp, sep = " — ") %>%
-      gather(key = "stat", value = "value", Freq, Prop, CI) %>%
-      unite(var_stat, !! colvar, stat, sep = "_") %>%
-      spread(key = var_stat, value = value) %>%
-      select(!! rowvar, No_Freq, No_Prop, No_CI, Yes_Freq, Yes_Prop, Yes_CI) %>%
-      rownames_to_column()
-  }
-  
-  
+  # p-value
   p <- svychisq(formula, 
                 df, 
                 na.rm = T, 
@@ -215,6 +229,7 @@ recap_bi_catvar_svy <- function(svy, var, byvar, percent = "col") {
     mutate(p.value = format(round(p.value, 4), nsmall = 4)) %>%
     rownames_to_column()
   
+  # Putting all the pieces together
   tab <- overall_tab %>%
     left_join(by_tab, by = c("rowname", rowvar_str)) %>%
     left_join(p, by = "rowname") %>%
@@ -225,6 +240,92 @@ recap_bi_catvar_svy <- function(svy, var, byvar, percent = "col") {
   return(tab)
   
 }
+
+
+# This function returns a table that summarizes variables overall
+# and by another variable, using survey data. It also performs
+# The chi-square test uses the svychisq() function from the 
+# survey package. It computes first and second-order Rao-Scott 
+# corrections to the Pearson chisquared test (defaults "F")
+# This function returns frequencies (Freq) and proportions (Prop)
+# and 95% intervals 
+# The percent argument specifies if you want the percents to be as 
+# a fraction of the column variable ("col") or row variable ("row")
+
+
+# recap_bi_catvar_svy <- function(svy, var, byvar, percent = "col") {
+#   
+#   df <- svy
+#   rowvar <- enquo(var)
+#   colvar <- enquo(byvar)
+#   rowvar_str <- quo_name(rowvar)
+#   colvar_str <- quo_name(colvar)
+#   formula <- as.formula(paste("~", rowvar_str, "+", colvar_str))
+#   
+#   overall_tab <- df %>%
+#     group_by(!! rowvar, .drop = FALSE) %>%
+#     summarise(Freq = unweighted(n()),
+#               Prop = survey_mean(na.rm = T,
+#                                  vartype = "ci")) %>%
+#     mutate_at(vars(starts_with("Prop")), 
+#               list(~format(round(. * 100, 1), nsmall = 1))) %>%
+#     unite(CI, Prop_low, Prop_upp, sep = " — ") %>%
+#     rownames_to_column() # Need to do this so can join p-value table (wrong number of rows)
+#                          # for bind_cols
+#   
+#   if(percent == "col") {
+#     
+#     by_tab <- df %>%
+#       group_by(!! colvar, !! rowvar, .drop = FALSE) %>%
+#       summarise(Freq = unweighted(n()),
+#                 Prop = survey_mean(na.rm = T,
+#                                    vartype = "ci")) %>%
+#       mutate_at(vars(starts_with("Prop")), 
+#                 list(~format(round(. * 100, 1), nsmall = 1))) %>%
+#       unite(CI, Prop_low, Prop_upp, sep = " — ") %>%
+#       gather(key = "stat", value = "value", Freq, Prop, CI) %>%
+#       unite(var_stat, !! colvar, stat, sep = "_") %>%
+#       spread(key = var_stat, value = value) %>%
+#       select(!! rowvar, No_Freq, No_Prop, No_CI, Yes_Freq, Yes_Prop, Yes_CI) %>%
+#       rownames_to_column() 
+#     
+#   } else {
+#     
+#     by_tab <- df %>%
+#       group_by(!! rowvar, !! colvar, .drop = FALSE) %>%
+#       summarise(Freq = unweighted(n()),
+#                 Prop = survey_mean(na.rm = T,
+#                                    vartype = "ci")) %>%
+#       mutate_at(vars(starts_with("Prop")), 
+#                 list(~format(round(. * 100, 1), nsmall = 1))) %>%
+#       unite(CI, Prop_low, Prop_upp, sep = " — ") %>%
+#       gather(key = "stat", value = "value", Freq, Prop, CI) %>%
+#       unite(var_stat, !! colvar, stat, sep = "_") %>%
+#       spread(key = var_stat, value = value) %>%
+#       select(!! rowvar, No_Freq, No_Prop, No_CI, Yes_Freq, Yes_Prop, Yes_CI) %>%
+#       rownames_to_column()
+#   }
+#   
+#   
+#   p <- svychisq(formula, 
+#                 df, 
+#                 na.rm = T, 
+#                 statistic = "adjWald") %>%
+#     tidy() %>%
+#     select(p.value) %>%
+#     mutate(p.value = format(round(p.value, 4), nsmall = 4)) %>%
+#     rownames_to_column()
+#   
+#   tab <- overall_tab %>%
+#     left_join(by_tab, by = c("rowname", rowvar_str)) %>%
+#     left_join(p, by = "rowname") %>%
+#     select(-rowname) %>%
+#     data.frame(row.names = 1, fix.empty.names = F) %>%
+#     as.matrix()
+#   
+#   return(tab)
+#   
+# }
 
 # This function returns a table that summarizes variables overall
 # and by another variable, using survey data. It also performs
